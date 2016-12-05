bdc0e752eb4eb9cf842e9afa7c5552016dc8a822
slawiko <shchaurouski.slava@gmail.com>
2016-12-05 10:00:12 +0300

    refactoring

--- criterias.py
+++ criterias.py
@@ -2,27 +2,27 @@ import math
 
 
 def kolmogorov(generator):
-	generator = sorted(generator)
-	n = len(generator)
-	D = 0
-	for i in range(n):
-		diff = abs((i + 1) / n - generator[i])
-		D = diff if D < diff else D
-	return D * math.sqrt(n)
+    generator = sorted(generator)
+    n = len(generator)
+    D = 0
+    for i in range(n):
+        diff = abs((i + 1) / n - generator[i])
+        D = diff if D < diff else D
+    return D * math.sqrt(n)
 
 
 def pirson(generator, k):
-	n = len(generator)
-	size = 1 / k
-	down, up = 0, 0 + size
-	exp_count = n / k
-	real_count = 0
-	hi2 = 0
-	for i in range(k):
-		for g in generator:
-			if down <= g < up:
-				real_count += 1
-		hi2 += ((real_count - exp_count) ** 2) / exp_count
-		real_count = 0
-		down, up = up, round(up + size, 15)  # bullshit
-	return hi2
+    n = len(generator)
+    size = 1 / k
+    down, up = 0, 0 + size
+    exp_count = n / k
+    real_count = 0
+    hi2 = 0
+    for i in range(k):
+        for g in generator:
+            if down <= g < up:
+                real_count += 1
+        hi2 += ((real_count - exp_count) ** 2) / exp_count
+        real_count = 0
+        down, up = up, round(up + size, 15)  # bullshit
+    return hi2
--- generators.py
+++ generators.py
@@ -2,48 +2,48 @@ import random
 
 
 class MultiplicativeCongruentialGenerator:
-	def __init__(self):
-		self.seq = []
+    def __init__(self):
+        self.seq = []
 
-	def generate(self, n, beta=78125, m=2 ** 31, start=78125, cache=False):
-		if cache and len(self.seq) == n:
-			return self.seq
-		temp = list()
-		temp.append(start)
-		self.seq = list()
-		for i in range(n):
-			self.seq.append(temp[i] / m)
-			temp.append((beta * temp[i]) % m)
-		return self.seq
+    def generate(self, n, beta=78125, m=2 ** 31, start=78125, cache=False):
+        if cache and len(self.seq) == n:
+            return self.seq
+        temp = list()
+        temp.append(start)
+        self.seq = list()
+        for i in range(n):
+            self.seq.append(temp[i] / m)
+            temp.append((beta * temp[i]) % m)
+        return self.seq
 
 
 class Random:
-	def __init__(self):
-		self.seq = []
+    def __init__(self):
+        self.seq = []
 
-	def generate(self, n, cache=False):
-		if cache and len(self.seq) == n:
-			return self.seq
-		self.seq = list()
-		for i in range(n):
-			self.seq.append(random.random())
-		return self.seq
+    def generate(self, n, cache=False):
+        if cache and len(self.seq) == n:
+            return self.seq
+        self.seq = list()
+        for i in range(n):
+            self.seq.append(random.random())
+        return self.seq
 
 
 class MacLarenMarsaglia:
-	def __init__(self):
-		self.seq = []
+    def __init__(self):
+        self.seq = []
 
-	def generate(self, generator1, generator2, cache=False):
-		if len(generator1) is not len(generator2):
-			raise Exception('Generators must have the same len')
-		if cache and len(self.seq) == len(generator1):
-			return self.seq
-		K = len(generator1)
-		V = list(generator1)
-		self.seq = list()
-		for i in range(K):
-			s = int(generator2[i] * K)
-			self.seq.append(V[s])
-			V[s] = generator1[i]
-		return self.seq
+    def generate(self, generator1, generator2, cache=False):
+        if len(generator1) is not len(generator2):
+            raise Exception('Generators must have the same len')
+        if cache and len(self.seq) == len(generator1):
+            return self.seq
+        K = len(generator1)
+        V = list(generator1)
+        self.seq = list()
+        for i in range(K):
+            s = int(generator2[i] * K)
+            self.seq.append(V[s])
+            V[s] = generator1[i]
+        return self.seq
